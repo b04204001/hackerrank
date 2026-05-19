1b2a0a27f7233cfbe4346ee9bed47f7a3b35f1f8
b04204001 <60873981+b04204001@users.noreply.github.com>
2026-05-19 23:35:48 +0800

    Update Array_Manipulation.cpp

--- Array_Manipulation.cpp
+++ Array_Manipulation.cpp
@@ -1,6 +1,6 @@
 long arrayManipulation(int n, vector<vector<int>> queries) {
     // O(m) 當成上下車，start +上數值，q[1]不減一是因為，調整到q[1]-1，所以q[1]操作-
-    vector<int> diff(n,0); 
+    vector<int> diff(n + 1,0); //n + 1 是否只剛好碰到n會越界，
     for(auto q : queries){
         int start = q[0] - 1; //對齊0~n-1
         int end = q[1] ; //因為q[1]-1仍受影響，放在q[1]去扣
