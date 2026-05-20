8a28044ee751d18b0a473993256f1972ba231a18
b04204001 <60873981+b04204001@users.noreply.github.com>
2026-05-20 13:58:05 +0800

    Update jump_on_clouds.cpp

--- jump_on_clouds.cpp
+++ jump_on_clouds.cpp
@@ -1,7 +1,9 @@
 int jumpingOnClouds(vector<int> c) {
     int n = c.size();
     vector<int> dp(n,0);
-    for(int i=1;i<n;i++){
+    dp[0] = 0;
+    dp[1] = (c[1] == 0) ? 1 : n;
+    for(int i=2;i<n;i++){
         if(c[i] == 0){
             dp[i] = min(dp[i-1] +1 , dp[i-2] + 1);
         }
